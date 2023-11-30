4986ebae9bb82d1365097dcde11fd7a68cb0c515
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-11-30 17:38:47 +0100

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,69 +1,73 @@
 import pl.mareklangiewicz.defaults.*
-import pl.mareklangiewicz.ure.*
+import pl.mareklangiewicz.deps.*
 import pl.mareklangiewicz.utils.*
 
 plugins {
-    id("io.github.gradle-nexus.publish-plugin") version vers.nexusPublishGradlePlugin
-    kotlin("multiplatform") version vers.kotlin apply false
+    plug(plugs.NexusPublish)
+    plug(plugs.KotlinMulti) apply false
+    plug(plugs.KotlinJvm) apply false
 }
 
-defaultGroupAndVerAndDescription(libs.RxMock)
-
-defaultSonatypeOssStuffFromSystemEnvs()
-
-private val rootBuild = rootProjectPath / "build.gradle.kts"
-private val rxmockModuleBuild = rootProjectPath / "rxmock" / "build.gradle.kts"
-private val kotlinSampleModuleBuild = rootProjectPath / "kotlinsample" / "build.gradle.kts"
-
-tasks.registerAllThatGroupFun("inject",
-    ::checkTemplates,
-    ::injectTemplates,
+defaultBuildTemplateForRootProject(
+    langaraLibDetails(
+        name = "RxMock",
+        description = "Tiny library for mocking RxJava calls.",
+        githubUrl = "https://github.com/langara/RxMock",
+        version = Ver(0, 0, 24)
+        // https://repo1.maven.org/maven2/pl/mareklangiewicz/rxmock/
+        // https://github.com/langara/RxMock/releases
+    ),
+    withSonatypeOssPublishing = true,
 )
 
-fun checkTemplates() {
-    checkRootBuildTemplate(rootBuild)
-    checkKotlinModuleBuildTemplates(rxmockModuleBuild, kotlinSampleModuleBuild)
-    checkMppModuleBuildTemplates(rxmockModuleBuild)
-    checkJvmAppBuildTemplates(kotlinSampleModuleBuild)
-}
-
-fun injectTemplates() {
-    injectRootBuildTemplate(rootBuild)
-    injectKotlinModuleBuildTemplate(rxmockModuleBuild, kotlinSampleModuleBuild)
-    injectMppModuleBuildTemplate(rxmockModuleBuild)
-    injectJvmAppBuildTemplate(kotlinSampleModuleBuild)
-}
-
 // region [Root Build Template]
+
+/** Publishing to Sonatype OSSRH has to be explicitly allowed here, by setting withSonatypeOssPublishing to true. */
+fun Project.defaultBuildTemplateForRootProject(
+    libDetails: LibDetails? = null,
+    withSonatypeOssPublishing: Boolean = false
+) {
+    check(libDetails != null || !withSonatypeOssPublishing)
+    ext.addDefaultStuffFromSystemEnvs()
+    libDetails?.let {
+        rootExtLibDetails = it
+        defaultGroupAndVerAndDescription(it)
+        if (withSonatypeOssPublishing) defaultSonatypeOssNexusPublishing()
+    }
+
+    // kinda workaround for kinda issue with kotlin native
+    // https://youtrack.jetbrains.com/issue/KT-48410/Sync-failed.-Could-not-determine-the-dependencies-of-task-commonizeNativeDistribution.#focus=Comments-27-5144160.0-0
+    repositories { mavenCentral() }
+}
 
 /**
  * System.getenv() should contain six env variables with given prefix, like:
  * * MYKOTLIBS_signing_keyId
  * * MYKOTLIBS_signing_password
- * * MYKOTLIBS_signing_key
+ * * MYKOTLIBS_signing_keyFile (or MYKOTLIBS_signing_key with whole signing key)
  * * MYKOTLIBS_ossrhUsername
  * * MYKOTLIBS_ossrhPassword
  * * MYKOTLIBS_sonatypeStagingProfileId
  * * First three of these used in fun pl.mareklangiewicz.defaults.defaultSigning
- * * See deps.kt/template-mpp/template-mpp-lib/build.gradle.kts
+ * * See DepsKt/template-mpp/template-mpp-lib/build.gradle.kts
  */
-fun Project.defaultSonatypeOssStuffFromSystemEnvs(envKeyMatchPrefix: String = "MYKOTLIBS_") {
-    ext.addAllFromSystemEnvs(envKeyMatchPrefix)
-    defaultSonatypeOssNexusPublishing()
-}
+fun ExtraPropertiesExtension.addDefaultStuffFromSystemEnvs(envKeyMatchPrefix: String = "MYKOTLIBS_") =
+    addAllFromSystemEnvs(envKeyMatchPrefix)
 
 fun Project.defaultSonatypeOssNexusPublishing(
-    sonatypeStagingProfileId: String = rootExt("sonatypeStagingProfileId"),
-    ossrhUsername: String = rootExt("ossrhUsername"),
-    ossrhPassword: String = rootExt("ossrhPassword"),
-) = nexusPublishing {
-    repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            stagingProfileId put sonatypeStagingProfileId
-            username put ossrhUsername
-            password put ossrhPassword
-            nexusUrl put uri(repos.sonatypeOssNexus)
-            snapshotRepositoryUrl put uri(repos.sonatypeOssSnapshots)
+    sonatypeStagingProfileId: String = rootExtString["sonatypeStagingProfileId"],
+    ossrhUsername: String = rootExtString["ossrhUsername"],
+    ossrhPassword: String = rootExtString["ossrhPassword"],
+) {
+    nexusPublishing {
+        this.repositories {
+            sonatype {  // only for users registered in Sonatype after 24 Feb 2021
+                stagingProfileId put sonatypeStagingProfileId
+                username put ossrhUsername
+                password put ossrhPassword
+                nexusUrl put repos.sonatypeOssNexus
+                snapshotRepositoryUrl put repos.sonatypeOssSnapshots
+            }
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,15 @@
-@file:Suppress("UnstableApiUsage")
-
-import okio.Path.Companion.toOkioPath
-import pl.mareklangiewicz.evts.*
-
-gradle.logSomeEventsToFile(rootProject.projectDir.toOkioPath() / "my.gradle.log")
-
 
 pluginManagement {
-    includeBuild("../deps.kt")
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
 }
 
 plugins {
-    id("pl.mareklangiewicz.deps.settings")
+    id("pl.mareklangiewicz.deps.settings") version "0.2.61" // https://plugins.gradle.org/search?term=mareklangiewicz
+    id("com.gradle.enterprise") version "3.15.1" // https://docs.gradle.com/enterprise/gradle-plugin/
 }
 
 rootProject.name = "RxMock"
